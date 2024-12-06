5d8c033101f7aa384c14044741e388a9d4258c50
abghifareihand <info.abghifareihan@gmail.com>
2024-12-06 15:50:34 +0700

    initial commit

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //index
+    public function index(Request $request)
+    {
+
+        //get product by request user id
+        $products = Product::with('user')->where('user_id', $request->user()->id)
+            ->orderBy('created_at', 'DESC')
+            ->get();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Product data loaded successfully',
+            'data' => $products
+        ]);
+    }
+
+    //get product by user id
+    public function getProductByUserId(string $userId)
+    {
+        $products = Product::with('user')->where('user_id', $userId)->get();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Product data loaded successfully',
+            'data' => $products
+        ]);
+    }
+
+    //store
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'price' => 'required|integer',
+            'stock' => 'required|integer',
+            'is_available' => 'required|boolean',
+            'is_favorite' => 'required|boolean',
+            'image' => 'required|image',
+        ]);
+
+        $user = $request->user();
+        $request->merge(['user_id' => $user->id]);
+
+        $data = $request->all();
+
+        $product = Product::create($data);
+
+        //check if image is available
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $image_name = time() . '.' . $image->getClientOriginalExtension();
+            $filePath = $image->storeAs('images/products', $image_name, 'public');
+
+            $product->image = $filePath;
+            $product->save();
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Product added successfully',
+            'data' => $product
+        ], 201);
+    }
+
+    //update
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'price' => 'required|integer',
+            'stock' => 'required|integer',
+            'is_available' => 'required|boolean',
+            'is_favorite' => 'required|boolean',
+        ]);
+
+        $product = Product::find($id);
+
+        if (!$product) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Product not found',
+            ], 404);
+        }
+
+        $data = $request->all();
+
+        $product->update($data);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Product updated successfully',
+            'data' => $product
+        ]);
+    }
+
+    //destroy
+    public function destroy(string $id)
+    {
+        $product = Product::find($id);
+
+        if (!$product) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Product not found',
+            ], 404);
+        }
+
+        $product->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Product deleted successfully',
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,63 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// user register
+Route::post('/user/register', [AuthController::class, 'userRegister']);
+
+// restaurant register
+Route::post('/restaurant/register', [AuthController::class, 'restaurantRegister']);
+
+// driver register
+Route::post('/driver/register', [AuthController::class, 'driverRegister']);
+
+// login
+Route::post('/login', [AuthController::class, 'login']);
+
+// update latlong user
+Route::put('/user/update-latlong', [AuthController::class, 'updateLatlong'])->middleware('auth:sanctum');
+
+// logout
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+// get all restaurant
+Route::get('/restaurant', [AuthController::class, 'getRestaurant']);
+
+// products
+Route::apiResource('/products', ProductController::class)->middleware('auth:sanctum');
+
+// get product by user id
+Route::get('/restaurant/{userId}/products', [ProductController::class, 'getProductByUserId'])->middleware('auth:sanctum');
+
+//order
+Route::post('/order', [OrderController::class, 'createOrder'])->middleware('auth:sanctum');
+
+//purchase order
+Route::post('/purchase/{orderId}', [OrderController::class, 'purchaseOrder'])->middleware('auth:sanctum');
+
+//get order by user id
+Route::get('/order/user', [OrderController::class, 'orderHistory'])->middleware('auth:sanctum');
+
+//get order by driver id
+Route::get('/order/driver', [OrderController::class, 'getOrdersByStatusDriver'])->middleware('auth:sanctum');
+
+//update order status
+Route::put('/order/restaurant/update-status/{id}', [OrderController::class, 'updateOrderStatus'])->middleware('auth:sanctum');
+
+//update order status driver
+Route::put('/order/driver/update-status/{id}', [OrderController::class, 'updateOrderStatusDriver'])->middleware('auth:sanctum');
+
+//update purchase status
+Route::put('/order/user/update-status/{id}', [OrderController::class, 'updatePurchaseStatus'])->middleware('auth:sanctum');
+
+//get order by restaurant id
+Route::get('/order/restaurant', [OrderController::class, 'getOrdersByRestaurantId'])->middleware('auth:sanctum');
